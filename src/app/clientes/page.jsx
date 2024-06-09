39127d1526200158d6daee063a3d62e76a1789de
WADEPAR6 <kparedes9203@uta.edu.ec>
2024-06-09 17:15:16 -0500

    IMPLEMENTACION - Implementacion de la tabla, boton de agreagar y metodos de eliminar en la pagina principal para poder realizar las acciones CRUD

--- src/app/clientes/page.jsx
+++ src/app/clientes/page.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
-import Navbar from '../../Components/navbar';
+"use client";
 
-const Page = () => {
-    return (
-        
-        <div>
-            <Navbar/>
-            <h1>Hello, World! CLIENTES</h1>
-        </div>
-    );
+import React from "react";
+import Navbar from "../../Components/navbar";
+import { useState } from "react";
+import Table from "../clientes/components/Table";
+import { useClients } from "../clientes/hooks/useClients";
+import ClientModal from "../clientes/components/Modal";
+
+const PageClient = () => {
+  const { clients, deleteClient, createClient, updateClient } = useClients();
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedClient, setSelectedClient] = useState(undefined);
+
+  const onClose = () => {
+    setIsOpen(false);
+    setSelectedClient(undefined);
+  };
+
+  const onEdit = (client) => {
+    setSelectedClient(client);
+    setIsOpen(true);
+  };
+
+  return (
+    <>
+      <div>
+        <Navbar />
+        <h1>Hello, World! CLIENTES</h1>
+      </div>
+
+      {isOpen && (
+        <ClientModal
+          isOpen={isOpen}
+          onClose={onClose}
+          onSubmit={selectedClient ? updateClient : createClient}
+          currentStudent={selectedClient}
+        />
+      )}
+      <div className="flex justify-normal w-screen">
+        <button
+          className="mt-5 w-2/12 ml-10 bg-purple-500 hover:bg-purple-900 text-white py-5 px-4 rounded text-xl font-bold"
+          onClick={() => setIsOpen(true)}
+        >
+          Agregar Cliente
+        </button>
+      </div>
+
+      <div className="flex py-8 justify-center items-center">
+        {clients && (
+          <Table clients={clients} onDelete={deleteClient} onEdit={onEdit} />
+        )}
+      </div>
+    </>
+  );
 };
 
-export default Page;
+export default PageClient;
